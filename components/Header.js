413c89e2f31def794e8c112e17a292534ff29bc8
eqan <eqan.ahmad123@gmail.com>
2022-04-21 20:59:35 +0500

    Resolving Resources Reachability Issues

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <Link href='/'>
           <a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
-            <Image src={`${process.env.NEXT_PUBLIC_FAVICON}/images/logo.png`} width={40} height={40} alt='logo' />
+            <Image src='/images/logo.png' width={40} height={40} alt='logo' />
             <span className='ml-3 text-xl'>Eqan Ahmad | Blogs</span>
           </a>
         </Link>
