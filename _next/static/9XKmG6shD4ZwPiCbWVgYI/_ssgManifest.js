c1ce133ca915090d985e14e7e62719f5e1f00c70
eqan <eqan.ahmad123@gmail.com>
2022-04-21 13:56:48 +0000

    Deploying to gh-pages from @ eqan/blogs@b8008066247a5149c4550a1b02c06dbd415a542b 🚀

---
+++ _next/static/9XKmG6shD4ZwPiCbWVgYI/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002F","\u002Fblog\u002F[slug]","\u002Fblog\u002Fcategory\u002F[category_name]","\u002Fblog\u002Fpage\u002F[page_index]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
